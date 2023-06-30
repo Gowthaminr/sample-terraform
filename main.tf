b379b7dd31a57543e64d2644bbef19f2e31964b3
Gowthaminr <84511384+Gowthaminr@users.noreply.github.com>
2023-06-30 11:54:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5BMGLEQ53NWZAD7L"
-  secret_key = "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"
+  access_key = "AKIASZDAANNMGMTKAPC5"
+  secret_key = "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"
 }
 resource "aws_iam_role" "lambda_role" {
 name   = "Spacelift_Test_Lambda_Function_Role"
